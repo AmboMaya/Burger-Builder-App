a4a3582aebd419b16c4d50cdd52b247cfee4b9b4
Ice Maya <echaymaya@gmail.com>
2019-01-28 14:05:14 +1300

    added feature to diable less button at the start/ added order button/ added current price info

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import classes from './BuildControl.css'
+
 const BuildControl = (props) => (
-  <div>
-    <div>{props.label}</div>
-    <button>Less</button>
-    <button>More</button>
+  <div className={classes.BuildControl}>
+    <div className={classes.label}>{props.label}</div>
+      <button className={classes.Less} onClick={props.removed} disabled={props.disabled}>Less</button>
+      <button className={classes.More} onClick={props.added}>More</button>
   </div>
 )
   
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import classes from './BuildControls.css'
 
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+import BuildControl from './BuildControl/BuildControl'
 
-const BuildControl = (props) => {
-  
-  return (
-    <div className={classes.Burger}>
-      
-    </div>
-  )
-}
+const controls = [
+  {label: 'Salad', type:'salad'},
+  {label: 'Bacon', type:'bacon'},
+  {label: 'Cheese', type:'cheese'},
+  {label: 'Meat', type:'meat'},
+]
 
-export default BuildControl
+const BuildControls = (props) => (
+  <div className={classes.BuildControls}>
+    <p>Current price: <strong>{props.price.toFixed(2)}</strong></p>
+    {controls.map(ctrl => (
+      <BuildControl key={ctrl.label}
+        label={ctrl.label} 
+        added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)} 
+        disabled={props.disabled[ctrl.type]}
+        />
+    ))}
+    <button className={classes.OrderButton} disabled={!props.purchasable}>Order Now</button>
+  </div>
+)
+
+export default BuildControls
